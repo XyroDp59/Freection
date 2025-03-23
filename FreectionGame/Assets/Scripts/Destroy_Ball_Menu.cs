5a3b7f901434b7f436f83e3815e4ee58d8685c41
Titouan DELEPORTE <titouandeleporte@icloud.com>
2025-03-23 14:10:47 +0100

    Main Menu
    Modification de la destruction des boules

--- FreectionGame/Assets/Scripts/Destroy_Ball_Menu.cs
+++ FreectionGame/Assets/Scripts/Destroy_Ball_Menu.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnBall : MonoBehaviour
+public class Destroy_Ball_Menu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject,12f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.other.gameObject);
     }
 }
